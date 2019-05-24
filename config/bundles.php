c8724ee890ee94a80e0902bfa94e5fb2c07d4778
bdemalglaive <bdemalglaive@sedona.fr>
2019-05-24 17:12:44 +0200

    [Sensio] The Symfony version should be the latest stable one

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
