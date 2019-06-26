f47f5d61b50a7836596b07f61d5fdad349acc59e
Elodie Amorim <eamorim@sedona.fr>
2019-06-26 16:39:01 +0200

    Install Fonts + Integration Header

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var Encore = require('@symfony/webpack-encore');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 Encore
     // directory where compiled assets will be stored
@@ -63,6 +64,10 @@ Encore
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
     //.addEntry('admin', './assets/js/admin.js')
+
+    .addPlugin(new CopyWebpackPlugin([
+        { from: './assets/fonts', to: 'fonts' }
+    ]))
 ;
 
 module.exports = Encore.getWebpackConfig();
