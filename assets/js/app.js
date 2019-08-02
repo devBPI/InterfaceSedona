32877cc9b479e01ff5658d55e752ca00181d3576
Elodie Amorim <eamorim@sedona.fr>
2019-08-02 11:26:44 +0200

    Ajout slick carousel + integration carousel home & parcours

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,10 +12,60 @@ require('../css/app.scss');
 const $ = require('jquery');
 
 require('bootstrap');
+require('slick-carousel');
 
 console.log('test');
 
 $('[data-toggle="tooltip"]').tooltip({
 	trigger: 'click',
 	template: '<div class="tooltip" role="tooltip"><div class="tooltip-inner"></div></div>'
+});
+
+// Configuration Carousel Primary
+$('.js-carousel-primary').slick({
+	dots: true,
+	infinite: true,
+	slidesToShow: 4,
+	slidesToScroll: 4,    
+	autoplay: true,
+	autoplaySpeed: 5000,
+	prevArrow: '<button class="slick-prev" aria-label="Actualité précédente" type="button">Précédent</button>',
+	nextArrow: '<button class="slick-next" aria-label="Actualité suivante" type="button">Suivant</button>',
+	dotsClass: 'carousel__pagination',
+					customPaging: function (slider, i) {
+					var slideNumber = (i + 1),
+						totalSlides = slider.slideCount;
+						return '<a class="carousel__pagination-dot" href="#" type="button" role="tab"><span class="sr-only">' + slideNumber + ' page sur ' + totalSlides + '</span></a>';
+					},
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: 	{
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 576,
+			settings: 	{
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+	]
+});
+
+// Gestion Bouton "Pause/Play"
+$('.carousel__button').on('click', function() {
+	if( $(this).hasClass('carousel__button--pause') ) {
+		$('.js-carousel-primary').slick('slickPause');
+		$(this).attr('aria-label', 'Mettre en lecture le carousel');
+		$(this).removeClass('carousel__button--pause')
+		$(this).addClass('carousel__button--play');
+	} else {
+		$('.js-carousel-primary').slick('slickPlay');
+		$(this).attr('aria-label', 'Mettre en pause le carousel');
+		$(this).removeClass('carousel__button--play')
+		$(this).addClass('carousel__button--pause');
+	}
 });
