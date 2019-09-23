6e5d785aeeb7331ce39b0384b509fb3df58f8f49
gmeot <gmeot@sedona.fr>
2019-09-23 15:34:31 +0200

    Remove ldap install in docker

--- .deploy/dev/app/run.sh
+++ .deploy/dev/app/run.sh
@@ -7,8 +7,3 @@ if ! [ -x "$(command -v wkhtmltopdf)" ]; then
   echo 'Install wkhtmltopdf and xvfb'
   apt-get install -y xvfb wkhtmltopdf
 fi
-
-if ! [ -x "$(command -v libldap2-dev)" ]; then
-  echo 'Install libldap2 and ldap php extension'
-  apt-get update && apt-get install libldap2-dev -y && docker-php-ext-install ldap
-fi
