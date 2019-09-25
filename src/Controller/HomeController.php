e509fa855785d8e529738f1fdad5d067f8f18a74
Anas Foullous <afoullous@sedona.fr>
2019-09-25 18:41:49 +0200

    mis en place de la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Thematic;
 use App\Service\Provider\CarouselProvider;
+use Doctrine\Common\Collections\ArrayCollection;
 use JMS\Serializer\Exception\XmlErrorException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -38,12 +40,14 @@ class HomeController extends AbstractController
         $attr = [];
         try {
             $attr['carousel'] = $this->carouselProvider->getHomeList();
+
         } catch (XmlErrorException $exception) {
         }
+        $thematic = $this->getDoctrine()->getRepository(Thematic::class)->findAll();
 
         return $this->render(
             'home/default.html.twig',
-            $attr
+            $attr+['thematic'=>$thematic]
         );
     }
 
@@ -63,9 +67,11 @@ class HomeController extends AbstractController
         } catch (XmlErrorException $exception) {
         }
 
+        $thematic = $this->getDoctrine()->getRepository(Thematic::class)->findOneBy(['slug'=>$thematic]);
+
         return $this->render(
             'home/thematic.html.twig',
-            $attr
+            $attr + ['thematic'=>$thematic]
         );
     }
 }
