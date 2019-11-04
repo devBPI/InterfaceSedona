02e7c43581a9b093548ed3fa3da76b918efb49f9
gmeot <gmeot@sedona.fr>
2019-11-04 14:45:37 +0100

    Login message error

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,7 +4,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 /**
  * Class UserController
@@ -14,18 +17,28 @@ class UserController extends AbstractController
 {
     /**
      * @Route("/authentification", methods={"GET","POST"}, name="user_login")
+     * @param AuthenticationUtils $authUtils
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function loginAction()
+    public function loginAction(AuthenticationUtils $authUtils): Response
     {
-        return $this->render('user/login.html.twig');
+        $authenticationError = $authUtils->getLastAuthenticationError();
+        $attr = [];
+        if ($authenticationError instanceof AuthenticationException) {
+            $attr = [
+                'error' => $authenticationError->getMessage(),
+                'last_login' => $authUtils->getLastUsername()
+            ];
+        }
+
+        return $this->render('user/login.html.twig', $attr);
     }
 
     /**
      * @Route("/compte", methods={"GET","HEAD"}, name="user_personal_data")
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function personalDataAction()
+    public function personalDataAction(): Response
     {
         return $this->render('user/personal-data.html.twig');
     }
