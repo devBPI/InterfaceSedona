3517160024e478b3dc1f021039a8d05e27df590b
bdemalglaive <bdemalglaive@sedona.fr>
2019-11-04 15:31:28 +0100

    Ajoute de l'affichage du numero de version

---
+++ src/Twig/CustomResourceExtension.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Twig;
+
+use Symfony\Component\Filesystem\Filesystem;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Twig\TwigFilter;
+
+/**
+ * Class CustomResourceExtension
+ * @package AppBundle\Twig
+ */
+class CustomResourceExtension extends AbstractExtension
+{
+
+    /**
+     * @var Filesystem
+     */
+    protected $fs;
+
+    /**
+     * @var string
+     */
+    protected $root_dir;
+
+    /**
+     * CustomResourceExtension constructor.
+     * @param Filesystem $fs
+     * @param $root_dir
+     */
+    public function __construct(Filesystem $fs, $root_dir)
+    {
+
+        $this->fs = $fs;
+        $this->root_dir = $root_dir."/../";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('get_package_info',[$this, 'getPackageInfo'])
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('file_exist', [$this, 'fileExistFilter']),
+            new TwigFilter('file_get_content', [$this, 'fileGetContentFilter'])
+        ];
+    }
+
+    /**
+     * @param $path
+     * @return bool
+     */
+    public function fileExistFilter($path)
+    {
+        return $this->fs->exists($this->root_dir.$path);
+    }
+
+    /**
+     * @param $path
+     * @return string
+     */
+    public function fileGetContentFilter($path)
+    {
+        return $this->fileExistFilter($path) ? file_get_contents($this->root_dir.$path) : "";
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPackageInfo()
+    {
+        $content = $this->fileExistFilter('package_info.json') ? file_get_contents($this->root_dir.'package_info.json') : "{}";
+        return json_decode($content, true);
+    }
+
+}
