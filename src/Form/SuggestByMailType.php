d7009fafe505da8c43d8a7f85b2cfe440f54d73d
Anas Foullous <afoullous@sedona.fr>
2019-11-07 18:07:34 +0100

    temp

--- src/Form/SuggestByMailType.php
+++ src/Form/SuggestByMailType.php
@@ -30,7 +30,7 @@ class SuggestByMailType extends AbstractType
                 'required'  => true,
             ])
             ->add('author', TextareaType::class,[
-                'required'  => false,
+                'required'  => true,
             ])
             ->add('documentType', ChoiceType::class,[
                     'required'=> true,
@@ -49,8 +49,10 @@ class SuggestByMailType extends AbstractType
                 'required'  => false
             ])
             ->add('email', EmailType::class,[
-                'required'  => false,
-                'attr'      => ['autocomplete'=> 'email' ]
+                'required'  =>  false,
+                'attr'      =>  ['autocomplete'=> 'email' ],
+                'invalid_message' => "wesh le mail la t'es sérieux ?"
+
             ])
         ;
     }
