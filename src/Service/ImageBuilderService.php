e0735242c6d42aab8cc690946f9a0fe3028b1d98
Sébastien Collado <scollado@sedona.fr>
2019-12-07 22:00:58 +0100

    Code review & refacto

--- src/Service/ImageBuilderService.php
+++ src/Service/ImageBuilderService.php
@@ -14,25 +14,24 @@ final class ImageBuilderService
 {
     use TraitSlugify;
 
-    public  const PARENT_FOLDER             = 'imported_images';
-    public  const COVER_FOLDER              = 'couvertures-generiques';
-    private const IMAGE_FOLDER              = 'images';
-    public const BPI_FOLDER_NAME_ELECTRE    = 'electre';
-    public const DEFAULT_PICTURE            = 'couvertures-generiques/cg-%s.svg';
+    private const IMAGE_FOLDER = 'images';
+    public const PARENT_FOLDER = 'imported_images';
+    public const BPI_FOLDER_NAME_ELECTRE = 'electre';
+    public const DEFAULT_PICTURE = 'couvertures-generiques/cg-%s.svg';
     public const THUMBNAIL = 'vignette';
-    public  const COVER = 'couverture';
-
-    /** @var string */
-    private $imageDir;
+    public const COVER = 'couverture';
     /**
      * @var string
      */
-    public  $url;
+    public $url;
+    /** @var string */
+    private $imageDir;
 
     /**
      * ImageBuilderService constructor.
+     *
      * @param string $imageDir
-     * @param $url
+     * @param        $url
      */
     public function __construct(string $imageDir, string $url)
     {
@@ -43,46 +42,101 @@ final class ImageBuilderService
     /**
      * @param string $content
      * @param string $type
+     *
+     * @param string $type
      * @return string
      */
-    public function buildImage(string $content, string $type='livre'): string
+    public function buildImage(string $content, string $type = 'livre'): string
     {
         $localFilePath = $this->slugify($type).DIRECTORY_SEPARATOR.$content;
+
         $fs = new Filesystem();
 
         if (!$fs->exists($this->imageDir.$localFilePath)) {
             $filename = $content;
 
-           try{
-               if (($content = file_get_contents($this->url.DIRECTORY_SEPARATOR.self::BPI_FOLDER_NAME_ELECTRE.DIRECTORY_SEPARATOR.$content)) === false){
-                   return $this->buildGenericPicture($type);
-               }
-           }catch (\ErrorException $e){
-                return $this->buildGenericPicture($type);
-           }
+            try {
+                $pictureURLparts = [$this->url, self::BPI_FOLDER_NAME_ELECTRE, $content];
+                $content = file_get_contents(implode(DIRECTORY_SEPARATOR, $pictureURLparts));
 
-           $fs->mkdir(str_replace($filename, '', $this->imageDir.self::PARENT_FOLDER.DIRECTORY_SEPARATOR.$localFilePath));
-           $this->saveLocalImage($content, $localFilePath);
+                if ($content === false) {
+                    return $this->buildGenericPicture($type);
+                }
+            } catch (\ErrorException $e) {
+                return $this->buildGenericPicture($type);
+            }
+
+            $fs->mkdir(
+                str_replace($filename, '', $this->imageDir.self::PARENT_FOLDER.DIRECTORY_SEPARATOR.$localFilePath)
+            );
+            $this->saveLocalImage($content, $localFilePath);
         }
 
         return $this->imageDir.self::PARENT_FOLDER.DIRECTORY_SEPARATOR.$localFilePath;
     }
 
     /**
+     * @param string      $path
+     * @param string|null $type
+     *
+     * @return null|string
+     */
+    public function getimage64(string $path, ?string $type = null): ?string
+    {
+        if ($type === null) {
+            $type = 'livre';
+        }
+        $prefix = sprintf('imported_images%s%s%1$s', DIRECTORY_SEPARATOR, $type);
+        $filePath = $this->buildImage(
+            str_replace($prefix, '', $path),
+            $type
+        );
+        $file = null;
+        try {
+            $file = new File($filePath, true);
+
+            if (isset($file) && (!$file->isFile() || 0 !== strpos($file->getMimeType(), 'image/'))) {
+                return null;
+            }
+        } catch (\Exception $e) {
+            return null;
+        }
+
+        $binary = file_get_contents($filePath);
+
+        if (false === $binary) {
+            return null;
+        }
+
+        return sprintf('data:image/%s;base64,%s', $file->guessExtension(), base64_encode($binary));
+    }
+
+    /**
      * @param string $type
+     *
      * @return string
      */
-    private function buildGenericPicture(string $type)
+    private function buildGenericPicture(string $type): string
     {
         $fs = new Filesystem();
-        $filePath = self::PARENT_FOLDER.DIRECTORY_SEPARATOR.$this->slugify($type).DIRECTORY_SEPARATOR.self::COVER_FOLDER.DIRECTORY_SEPARATOR.'cg-'.$this->slugify($type).'.svg';
-        if ($fs->exists($filePath)){
+
+        $filePath = self::PARENT_FOLDER.DIRECTORY_SEPARATOR;
+        $filePath .= $this->slugify($type).DIRECTORY_SEPARATOR;
+        $filePath .= sprintf(self::DEFAULT_PICTURE, $this->slugify($type));
+
+        if ($fs->exists($filePath)) {
             return $filePath;
         }
-        $fs->copy( self::IMAGE_FOLDER.DIRECTORY_SEPARATOR.self::COVER_FOLDER.DIRECTORY_SEPARATOR.'cg-'.$this->slugify($type).'.svg', $filePath);
+        $fs->copy(
+            self::IMAGE_FOLDER
+            .DIRECTORY_SEPARATOR
+            .sprintf(self::DEFAULT_PICTURE, $this->slugify($type)),
+            $filePath
+        );
 
         return $filePath;
     }
+
     /**
      * Save file in local from content
      *
@@ -93,33 +147,6 @@ final class ImageBuilderService
     {
         $fs = new Filesystem();
         $fs->dumpFile($this->imageDir.self::PARENT_FOLDER.DIRECTORY_SEPARATOR.$localPath, $content);
-    }
-
-    /**
-     * @param string $path
-     * @param string|null $type
-     * @return null|string
-     */
-    public function getimage64(string $path, string $type=null)
-    {
-        if ($type===null){
-            $type='livre';
-        }
-        $filePath = $this->buildImage(str_replace("imported_images".DIRECTORY_SEPARATOR.$type.DIRECTORY_SEPARATOR,"", $path), $type);
-        $file = null;
-        try {
-            $file = new File($filePath, true);
-        } catch (\Exception $e) {
-
-        }
-
-        if (isset($file) && (!$file->isFile() || 0 !== strpos($file->getMimeType(), 'image/'))) {
-            return null;
-        }
-
-         $binary = file_get_contents($filePath);
-
-        return sprintf('data:image/%s;base64,%s', $file->guessExtension(), base64_encode($binary));
     }
 }
 
