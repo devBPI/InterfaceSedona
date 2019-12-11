855dbe910f3094bf1ea29add8ea8bf0ab01a4574
gmeot <gmeot@sedona.fr>
2019-12-11 14:36:13 +0100

    #24191 : table accessibility

--- assets/js/datatable.js
+++ assets/js/datatable.js
@@ -1,13 +1,13 @@
-require('datatables.net-bs4');
+require('datatables.net/js/jquery.dataTables.min.js');
 
 $(document).ready(function () {
     $('table.table-striped').dataTable({
         "language": {
           "emptyTable": "Il n'y a aucun document dans votre liste"
         },
+        "tabIndex": '-1',
         "paging":   false,
         "info":   false,
-        "searching":   false,
-        "scrollX": true
+        "searching":   false
 	});
 });
