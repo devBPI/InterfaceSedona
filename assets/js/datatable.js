514ffa7ca0915f8e4ff1d54b03a8e56a9b477c1c
Elodie Amorim <eamorim@sedona.fr>
2020-01-22 17:19:28 +0100

    RGAA : #24191 fix aria label table ma selection

--- assets/js/datatable.js
+++ assets/js/datatable.js
@@ -3,7 +3,11 @@ require('datatables.net/js/jquery.dataTables.min.js');
 $(document).ready(function () {
     $('table.table-striped').dataTable({
         "language": {
-          "emptyTable": "Il n'y a aucun document dans votre liste"
+          "emptyTable": "Il n'y a aucun document dans votre liste",
+          "aria": {
+            "sortAscending": ": activer pour trier par ordre ascendant",
+            "sortDescending": ": activer pour trier par ordre descendant"
+          }
         },
         "tabIndex": '-1',
         "paging":   false,
