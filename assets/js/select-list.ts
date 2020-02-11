3fc655da89ecf000f3bf69f27298f22c7794b037
Elodie Amorim <eamorim@sedona.fr>
2020-02-11 15:43:03 +0100

    RGAA : #24249 fix select2

--- assets/js/select-list.ts
+++ assets/js/select-list.ts
@@ -13,27 +13,43 @@ export default class SelectList {
             formatNoMatches: (term: string) => {
                 return 'Aucune langue trouvée pour la saisie '+term;
             }
-        }).on('select2-opening select2-close', (e) => {
+        })
+        .on('select2-opening select2-close', (e) => {
             let generatedElement = document.querySelector('input.select2-input') as HTMLInputElement;
             if (generatedElement) {
-                generatedElement.setAttribute('aria-expanded', e.type == 'select2-opening'?'true':'false');
+                generatedElement.setAttribute('aria-expanded', (e.type == 'select2-opening') ? 'true' : 'false');
+                this.updateTotalElement(generatedElement);
             }
+        })
+        .on('select2-removed', (e) => {
+            let generatedElement = document.querySelector('input.select2-input');
+            if (generatedElement) {
+                this.updateTotalElement(generatedElement);
+            }
+        })
+        .on('select2-open', () => {
+            let object = $(this).data('select2');
+            object.select.before(object.liveRegion);
         });
 
         setTimeout(() => {
-            let generatedListId = 'select2-adv-search-langage-results';
-            let generatedList = document.querySelector('ul.select2-choices') as HTMLUListElement;
-            if (generatedList) {
-                generatedList.setAttribute('id', generatedListId);
-            }
-
-            let generatedElement = document.querySelector('input.select2-input') as HTMLInputElement;
-            if (generatedElement) {
-                generatedElement.setAttribute('role', 'listbox');
-                generatedElement.setAttribute('aria-controls', generatedListId);
-                generatedElement.setAttribute('aria-expanded', 'false');
-            }
+            $("[data-toggle=select2]").each( (el) => {
+                let $this = $(this), 
+                    generatedListId = 'select2-adv-' + $this.attr('id'),
+                    object = $this.data('select2');
+                object.selection.attr('id', generatedListId);
+                object.search
+                    .attr('role', 'button')
+                    .attr('aria-controls', generatedListId)
+                    .attr('aria-expanded', 'false');
+                    this.updateTotalElement(object.search);
+            });
         }, 0);
     }
 
+    private updateTotalElement(list:any) {
+        let countChoices = document.querySelector('ul.select2-choices').children.length - 1;
+
+        list.setAttribute('aria-label', countChoices + ' éléments sélectionnés');
+    }
 }
