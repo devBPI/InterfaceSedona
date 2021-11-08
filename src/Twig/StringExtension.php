9665f6b7f367bad35a7cdc7b679ba397dc629290
bdemalglaive <bdemalglaive@sedona.fr>
2021-11-08 16:33:46 +0100

    Fix sur les dépendances composer

--- src/Twig/StringExtension.php
+++ src/Twig/StringExtension.php
@@ -23,6 +23,7 @@ class StringExtension extends AbstractExtension
             new TwigFilter('snake', [$this, 'toSnakeCase']),
             new TwigFilter('camel', [$this, 'toCamelCase']),
             new TwigFilter('slugify', [$this, 'toSlugify']),
+            new TwigFilter('wordwrap', [$this, 'wordwrap']),
         ];
     }
 
@@ -73,6 +74,19 @@ class StringExtension extends AbstractExtension
     public function toSnakeCase(string $text): string
     {
         return strtolower(ltrim(preg_replace('/([A-Z])/', '_\\1', $text), '_'));
+    }
+
+    /**
+     * @param string $text
+     * @return array
+     */
+    public function wordwrap(string $text,
+                             $width = 25,
+                             string $break = "\n",
+                             bool $cut_long_words = true
+    ): array
+    {
+        return explode($break, wordwrap($text,$width,$break,$cut_long_words));
     }
 
     /**
