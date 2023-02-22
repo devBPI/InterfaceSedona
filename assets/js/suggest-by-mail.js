65d4642925358e154eb565624e6d0206e00e28d5
namosdjoro <namosdjoro@sedona.fr>
2023-02-22 12:10:04 +0100

    add modal form

--- assets/js/suggest-by-mail.js
+++ assets/js/suggest-by-mail.js
@@ -1,13 +1,6 @@
-let modal = document.getElementById("modal-suggest");
-modal.addEventListener('hidden.bs.modal',  function(event) {
-    console.log(event);
-
-    let form = document.querySelector('#suggestion-form-remote')
-})
 $(document).on('hide.bs.modal', '[role=dialog]', function (event){
     var $this = $(this),
         $form =  $this.find('.modal-form');
-
     if ($form.length > 0) {
         $.get($form.attr('action'))
             .done(function (data) {
