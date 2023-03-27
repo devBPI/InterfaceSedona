7333ce956bb926bbda8142cc62fd57a5b831ce87
Benoit DE MALGLAIVE <bdemalglaive@sedona.fr>
2023-03-27 15:54:57 +0000

    Simplification de la config docker

--- .deploy/dev/app/run.sh
+++ .deploy/dev/app/run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+make install-wait-for-it
 make install-wkhtmltopdf
-#make assets
-#dpkg --configure -a
-
 make install-php-ext
+
+echo "\033[30;48;5;82m > Wait to Postgres ready -------------------------------------------------------- \033[0m"; \
+wait-for-it $POSTGRES_HOST:$POSTGRES_PORT -s -t 500 -- echo "        >  Postgres is ready"
+
+echo "\033[30;48;5;82m > Install project -------------------------------------------------------- \033[0m"; \
+if ! make install; then
+    echo "\033[30;48;5;196m make returned an error                                    \033[0m"; \
+fi
+echo "\033[30;48;5;82m > Install project done -------------------------------------------------------- \033[0m"; \
